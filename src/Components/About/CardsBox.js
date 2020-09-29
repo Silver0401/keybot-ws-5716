c78c73d37808dfef106bf8f4f877491b79ab287a
Ismael Muñoz <Prosilver05@gmail.com>
2020-09-29 18:02:53 -0500

    everything has now an english and spanish version

--- src/Components/About/CardsBox.js
+++ src/Components/About/CardsBox.js
@@ -30,34 +30,36 @@ import PaolaFoto from "../../Resources/paola.jpg";
 
 // Bios
 
-const IsmaBio = "Creador de la pagina. Ismael Muñoz Contreras, o mejor conocido como shelas, es actualmente un full stack web developer, con conocmientos avanzados de programación, electrónica y robótica. Asimismo, estudia actualmente la carrera de Medico Cirujano en el Tec de Monterrey campus Monterrey"
-const EmiBio = "Emiliano León Chavez: Shaggy para los amigos, me gusta programar, el arte y la salsa cheesy jalapeño con la corteza de la pizza. Si me ves algún día en el equipo puedes contar que te voy a ayudar."
-const MemoBio = "Guillermo Navarro - Tesorero 2017-2018 / Jefe de mecánica 2017-2018 / Mentor 2018-2019 / Inicie como un miembro de mecánica y poco a poco desarrolle una gran pasión por el equipo, aprendí más cosas de las que pense, desde diseño hasta un poco de administración al ser parte de la mesa directiva en 2017. Conocí a grandes personas y forme bellas amistades aquí, que aunque hemos tomado caminos diferentes, las sigo estimando demasiado. Tanto me lleno la experiencia en el grupo que pase de querer ser médico a ingeniero, y actualmente estudio robótica, Esta experiencia es una que simplemente no puedes dejar pasar."
-const EguzBio = "He sido un mentor de programación para la \"First Robotics Competition\" (FRC) del equipo #5716 desde 2015. FRC Siempre es un desafío, ya que al competir contra más inteligentes y experimentados creadores de robots en el mundo, te ves en la obligación de seguir programando y enseñando de una manera impecable. Eguzki fue el primer mentor de programación del equipo y es muy querido entre toda la comunidad de Keybot, el es la razón de que muchos jovenes hoy en día se entusiasmen por la robótica y la programación. Como una nota del creador de la página: Eguzki es el mejor mentor de programación que he tenido, pero más que eso siempre será mi amigo."
-const PaliBio = "Paola Elinora Parga Saucedo mejor conocida como Paly Parga es directora del área de Liderazgo y Vivencia en el Tecnológico de Monterrey. Tiene una licenciatura en administración de empresas por el Tecnológico de Monterrey, además de contar con una maestría y doctorado en Imagen Publica por el Colegio de consultores en Imagen Pública. Ella cree que todo es perfectible y que todo lo que se propongan se puede realizar con pasión. ";
-const SiwaBio = "Silvana Torres Ferrera estudia el 8vo semestre de contabilidad y finanzas. Su historia en Keybot comenzó en el 2014 cuando junto con algunos amigos fundaron el equipo. Para la temporada 2015 fue líder del equipo y al graduarse de preparatoria continuo como mentor Jr. en el área de administración. \"Keybot me enseñó a soñar y a llegar al mundial de robótica, definitivamente me cambió la vida y es increíble ver cómo años después le sigue cambiando la vida a las nuevas generaciones\""
-const DavoBio = "Para mí Keybot significa más que un equipo de robótica, es una familia que día a día crece más, una familia que al llegar a ella nunca deja de sorprendente, te enseña no sólo sobre tu área sino de la vida en general, te da competencias y habilidades que en ningún otro lado vas a encontrar, las personas con las que te encuentras pasan a convertirse de completos desconocidos a tus hermanos. Las experiencias que te deja a lo largo del camino siempre te enseñan algo y te vuelven mejor alumno y persona. Agradezco infinitamente a Keybot por lo que me enseñó a lo largo de mi prepa y me llevo todas las experiencias y sobre todo una gran familia a la que uno siempre va a pertenecer. Keybot es un cachito de mí.";
-const MagalyBio = "Licenciada en Mercadotecnia Internacional con experiencia en organización de eventos, promoción y administración de nómina, así como en creación y desarrollo del departamento de promoción de un Colegio en la Ciudad de México. Actualmente apoyo en redes sociales y desarrollo de nuevos productos en una empresa dulcera de México y participando como mentora en el área de mercadotecnia de ¡Keybot!"
-const LuisBio = "Luis Enrique Sánchez, profesor del departamento de Ciencias de PrepaTec. Mi historia en Keybot comenzó en el 2019 cuando me invitan a participar como Head Coach. En esa temporada tuvimos la oportunidad de quedar como finalistas en el regional de Campus Santa Fe, lo que nos dio el pase al Campeonato Mundial en Houston. \"Keybot me ha enseñado el gran potencial que tienen nuestros alumnos cuando persiguen sus sueños con pasión y disciplina\". "
-const OsbaldoBio = "Osbaldo Becerra, profesor del Tecnológico de Monterrey con una Maestría en ciencias en electrónica, diseño de circuitos integrados. Posgrado en el instituto nacional de astrofísica, óptica y electrónica (INAOE), en Tonantzinta, Puebla. Experiencia en diferentes áreas y empresas como electro fotónica y diseño electrónico en Axón, líder de ingeniería para creación del canal 26 del gobierno del estado de Puebla, en la transmisión de señal de tv en Once TV y catedrático en la Universidad politécnica de Pachuca. Actualmente apoya como mentor de electrónica al equipo de Keybot!"
-const SofiaBio = "Mi nombre es Sofía González, aunque me gusta más que me llamen Sofi. Tengo 16 años y soy coordinadora general en el equipo de robótica de Keybot. He probado todo tipo de deporte y baile, pero el que más me ha gustado es la danza irlandesa, en la cual asistí a varias competencias.";
-const AbiBio = "Mi nombre es Karla Abigail Henkel Bustamante. Soy la líder de finanzas. La mayoría me llama Aby o Karla. Me gusta bailar, cantar y jugar tenis.";
-// Rodrigo bio missing (temporal bio)
-const RodriBio = "Rodrigo Vázquez Hernández. Alumno de tercer semestre que ha formado parte del equipo durante todo su primer año de preparatoria. Ha estado en el área de administración y ha desempeñado tareas de mercadotecnia y scouting. Asistió al regional de Monterrey 2020. Ha participado en grupos estudiantiles como teateTie HidalMUN. Tiene un promedio de 97. ";
-const MariBio = "Mi nombre es Mariana, y me gusta que me digan así. Tengo 17 años y soy líder de patrocinios. Un fun fact mío es que me encantan las series de repostería.";
-const AleBio = "Me llamo Alessa, me gusta que me digan Ale y tengo 17 años. Soy la presidenta de la gestión 2020 - 2021 de Keybot. Considero que nuestro equipo, más que un equipo, es una familia. Un fun fact acerca de mi es que: adoro a los animales, a tal punto que he tenido desde perros y gatos, hasta conejos y patos. "
-const ItzelBio = "Soy Itzel Castellanos, tengo 17 años y soy líder de Chairman's, me gusta todo lo relacionado con las artes en especial la música y el dibujo, un fun fact sobre mi es que soy muy distraída con mis cosas, un día llegue a perder un solo zapato en el Schoolastico"
-const EugeBio = "Hola! Soy Eugenia De Jesus Rangel tengo 17 años. Soy la Líder del área de programación/electrónica y vicepresidenta del equipo para esta gestión de 2020 a 2021. Fun fact: Me gusta andar en motocicleta.";
-const NadiaBio = "Hola me llamo Nadia Belem, me gusta que me digan nadia y detesto el nombre de belem. Tengo 17 años y soy la lider del área de mercadotecnia de la gestión 2020 - 2021. Un random fact mio: Me gusta viajar mucho y adoro los chihuahas."
-// Bennet bio missing (temporal bio)
-const BennetBio = ""
-const KarlaBio = "Me llamo Karla Yenitzia Alducin Guzmán, líder de administración, tengo 17 años, mi segundo nombre es maya y significa segundo amanecer. He vivido en 9 estados de la república. Me gusta que me digan más por mi segundo nombre, pero no tengo ningún problema con ninguno de los dos. Mi fun fact es que cuando estoy nerviosa, me trueno los dedos de la mano";
-const MemeBio = "Estudiante del tecnológico de Monterrey campus Hidalgo,ex-Columbia,líder de neumática ,18 años de edad,le gusta el synthwave y odia la banda/reggaetón/cumbia Fun fact: me llaman meme (no pregunten)"
-const PaolaBio = "Me llamo Paola Salomón, pero me gusta que me digan Pao. Tengo 17 años. Soy líder de acción social de la gestión de keybot 2020-2021. Me gusta mucho jugar Tenis.";
-
 const CardsBox = () => {
 
   const {t} = useTranslation()
+
+  let IsmaBio = t("About.IsmaBio")
+  let EmiBio = t("About.EmiBio")
+  let MemoBio = t("About.MemoBio")
+  let EguzBio = t("About.EguzBio")
+  let PaliBio = t("About.PaliBio")
+  let SiwaBio = t("About.SiwaBio")
+  let DavoBio = t("About.DavoBio")
+  let MagalyBio = t("About.MagalyBio")
+  let LuisBio = t("About.LuisBio")
+  let OsbaldoBio = t("About.OsbaldoBio")
+  let SofiaBio = t("About.SofiaBio")
+  let AbiBio = t("About.AbiBio")
+// Rodrigo bio missing (temporal bio)
+  let RodriBio = t("About.RodriBio")
+  let MariBio = t("About.MariBio")
+  let AleBio = t("About.AleBio")
+  let ItzelBio = t("About.ItzelBio")
+  let EugeBio = t("About.EugeBio")
+  let NadiaBio = t("About.NadiaBio")
+// Bennet bio missing (temporal bio)
+  let BennetBio = t("About.BennetBio")
+  let MayaBio = t("About.MayaBio")
+  let SalmaBio = t("About.SalmaBio")
+  let KarlaBio = t("About.KarlaBio")
+  let MemeBio = t("About.MemeBio")
+  let PaolaBio = t("About.PaolaBio")
 
   const [MentorsCounter, changeMentorsCounter] = useState(0);
   const [CoachesCounter, changeCoachesCounter] = useState(0);
@@ -218,7 +220,7 @@ const CardsBox = () => {
   CreateCard("Emiliano L.C", EmiFoto, EmiBio, "Mentors", "card2");
   CreateCard("Guillermo", MemoFoto, MemoBio, "Mentors", "card3");
   CreateCard("Davo E.", davoFoto, DavoBio, "Mentors", "card7");
-  CreateCard("Salma", SalmaFoto, "missing bio", "Mentors", "card9");
+  CreateCard("Salma", SalmaFoto, SalmaBio, "Mentors", "card9");
   CreateCard("Silvana T.", siwaFoto, SiwaBio, "Mentors", "card10");
 
   CreateCard("Nadia B.", NadiaFoto, NadiaBio, "Leaders", "card20") 
@@ -239,7 +241,7 @@ const CardsBox = () => {
   CreateCard("Magaly", MagalyFoto, MagalyBio, "Coaches", "card16");
   CreateCard("Paola P.E.", PaliFoto, PaliBio, "Coaches", "card12");
   CreateCard("Luis S.G.", QuiquinFoto, LuisBio, "Coaches", "card11");
-  CreateCard("Carlos M.D.", mayaFoto, "missing bio", "Coaches", "card14");
+  CreateCard("Carlos M.D.", mayaFoto, MayaBio, "Coaches", "card14");
   CreateCard("Osbaldo B.", OsbaldoFoto, OsbaldoBio, "Coaches", "card15");
 
   return (
--- src/Components/About/FirstInfoBox.js
+++ src/Components/About/FirstInfoBox.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import anime from "animejs";
+import {useTranslation} from "react-i18next";
 
 //Img paths
 import AboutFirst from "./../../Resources/FIRSTlogo.png"
@@ -27,16 +28,18 @@ window.GameChangers = [DP2]
 
 // Global Paths for Seasons Info
 
-window.AboutFirstInfo =  "FIRST, which stands for: \"For Inspiration and Recognition of Science and Technology\". Is a nonprofit organization founded in 1989 by Dean Kamen. With the objective of inspiring young people's interest and participation in science and technology. FIRST has different leagues, in which we classify on the bigger one, FIRST Robotics Competition (FRC). It's more than a competition it's an event which combines the technology and the gracious professionalism to create an equity, diverse and inclusive environment that appreciates the technology and the science. Additionally, FIRST teams are encourage to spread the word, looking forward to enjoy, love and recognize the Science, Technology, Mathematics and Engineering."
-window.DeepSpaceInfo =  "Debido al Fiasco de la season pasada, en esta se optó por cambiar a las personas administrativas del equipo, es así como se cambiaron al coach y dueño del equipo. A partir de aquí se empezaron a notar cambios significativos, el equipo se habia vuelto más fuerte que nunca. Afortunadamente durante esta temporada también logramos pasar al mundial, pero esta vez con lo que se describe fue: El mejor robot que haya construido el equipo desde la creación del equipo. Fue sin lugar a duda uno, sino es que el mejor año para keybot y su desarrollo en todas las áreas, desde mecánica y programación hasta administración y labores sociales."
-window.RecycleRushInfo = "Fue la primera temporada del equipo, en donde todos admiten que nadie sabia nada. En aquellos tiempos chava empezo siendo el director de Keybot, mientras que Moises era el coach. Debido a la falta de conocmiento del equipo sobre basicamente todo, se opto por hacer un robot simple que solo buscara hacer uno de los retos. Se puede ver que desde el inicio Moises era fan de el uso de cadenas, puesto que ese año se empezaron usarlas y así siguio durante más de 3 temporadas. A pesar de ser la primera vez, el autónomo del robot era bastante bueno. Sin embargo al final, en la competencia el robot termino destruyendose, junto con sus sitema de recolección de cajas, por lo cual la alianza nos pidiío que convierteramos a nuestro robot en uno de pelea."
-window.SteamWorksInfo = "Steam works fue otra temporada en la que también se busco enforcarse en un solo mecanismo, el de los engranes, puesto que nuestro equipo aun no tenía ni los recuros ni los conocmientos para poder crear un robot integro. Además de esto se intento por primera vez implementar un segundo mecanismo, que fue el de elvación del robot, este consistía en una cuerda gruesa de velcro que se enrolllaba nuevamente con cadena para poder levantar al robot. Este también fue un buen año para el equipo de programación ya que, el autónomo tenía que ser muy preciso para poder funcionar, mucho equipos fallaron, sin embargo keybot salio adelante con algunas partidas en las cuales el autonomo fungió como la principal forma de anotar puntos para el equipo."
-window.StrongHoldInfo = "StrongHold en la opinion de la gran mayoría de los integrantes del equipo fue de las mejores temporadas, debido a que la temática era \"épica\". Durante esta temporada el equipo busco construir el robot más ligero posible debido a las pruebas terrestres que habia para este reto. Tambíen fue esta la primer temporada en la que intenamos utilizar visión de cámara en el robot. A pesar de que el mecanismo de lanzar pelotas era el único que tenía nuestro robot, este al igual que la temporada pasada dejo de funcionar y solo sirvió como un brazo para ayudar a los equipos de nuestras alianzas a superar ciertos obstáculos. Esta fue igual la primera vez que implementamos el scouting, en donde Montu hizo un trabajo extraordinario, ayudando a otros equipos a ganar únicamente mediante la observación de mecanismos de otros equipos y un análisis de la mejor posible estrategia."
-window.PowerUpInfo =  "Esta fue sin lugar a duda una de las temporadas con mayores fracasos en la construcción del robot, Ya que unas semanas antes del inicio de la competencia, todo nuestro robot se rompió, debido que la implementacion de cadena que se hizo, no fue suficiente para resistar el peso del brazo metálico que se supone agarraría cajas. Debido a esto se tuvo que hacer un pequeño robot en alrededor de 5 días, el cual llevamos a la competencia. No obstante, esta fue la primera vez que el equipo logró pasar al mundial, lo cual fue de las cosas más inesparadas que jamas le ocurrió al equipo. Sin embargo, después del mundial el equipo tuvo ciertos problemas puesto que no queríamos que en alguna otra temporada volviera a pasar lo mismo con el robot. Por lo cual hubo cierta tensión durante esta temporada."
-window.InfiniteRechargeInfo = "Debido a los reciente acontecimientos vistos por el COVID-19 esta temporada llego a su fin antes de lo esperado. Lo cual realmente es muy desafortunado para el equipo, puesto que se venía con un desempeño increible proveniente de las gestiones anteriores."
-window.GameChangersInfo = "El nuevo juego de FIRST a competir este año. Aun no estamos seguro de las diferencias que hay entre este e infinite recharge, sin embargo Keybot, siempre esta listo para dar lo mejor de cada uno de sus integrante, bajo cualquier circunstancia."
-
 const FirstInfoBox = (props) => {
+
+    const {t} = useTranslation()
+    
+    window.AboutFirstInfo = t("About.FirstInfo")
+    window.DeepSpaceInfo = t("About.DeepSpaceInfo")
+    window.RecycleRushInfo = t("About.RecycleRushInfo")
+    window.SteamWorksInfo = t("About.SteamWorksInfo")
+    window.StrongHoldInfo = t("About.StrongHoldInfo")
+    window.PowerUpInfo = t("About.PowerUpInfo")
+    window.InfiniteRechargeInfo = t("About.InfiniteRechargeInfo")
+    window.GameChangersInfo = t("About.GameChangersInfo")
 
     let link = "none"
     let imgSRC = window[props.ButtonValue][0]
