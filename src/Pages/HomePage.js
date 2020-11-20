eca46a918ad0cc96cf60e56a0a9d0d8d68c19a4c
Ismael Muñoz <Prosilver05@gmail.com>
2020-11-20 17:35:34 -0600

    added HomeMonitorOpt as well as a page loader

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React,{useEffect} from "react";
+import anime from "animejs";
 
 // Home Page Components
 import LeftBox from "./../Components/Home/LeftBox"
@@ -7,8 +8,45 @@ import RightBox from "./../Components/Home/RightBox"
 
 const HomePage = () => {
 
+
+    useEffect(() => {
+        window.onload = () => {
+            const Loader =  anime.timeline({
+                easing: "easeInOutSine",
+            })
+
+            Loader.add({
+                delay: 500,
+                targets: ".MovingBox",
+                duration: 2200,
+                translateY: ["120%", "-120%"]
+            })
+            
+            Loader.add({
+                targets: ".Loader",
+                duration: 750,
+                height: "0px"
+            }, "-=1700")
+        }
+        
+    },[])
+
     return(
         <div className="HomePage">
+
+            <div className="Loader">
+    
+                <span className="AnimBox">
+                    <div className="Box1"></div>
+                    <div className="Box2"></div>
+                    <div className="Box3"></div>
+                    <div className="Box4"></div>
+                </span>
+                <h1>Loading...</h1>
+
+                <div className="MovingBox"></div>
+
+            </div>
 
             <LeftBox/>
 
